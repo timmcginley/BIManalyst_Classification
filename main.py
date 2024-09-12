d396dc8249dd7aff242abaf1c73665b7a30d775f
Tim McGinley <timwasnever@gmail.com>
2024-09-12 14:00:28 +0200

    added uniclass (local link)

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import ifcopenshell
 #from rules import doorRule
 from rules import classificationRule
 
-model = ifcopenshell.open("C:/models/24_06/CES_BLD_24_06_ARC.ifc")
+# remember this is a local link on my machine as we cannnot upload the files to github.
+model = ifcopenshell.open("C:/models/24_06/CES_BLD_24_06_MEP.ifc")
 
 #windowResult = windowRule.checkRule(model)
 #doorResult = doorRule.checkRule(model)
@@ -12,4 +13,6 @@ classificationResult = classificationRule.checkRule(model)
 
 #print("Window result:", windowResult)
 #print("Door result:", doorResult)
+print('')
 print("Classification result:", classificationResult)
+print('')
--- rules/classificationRule.py
+++ rules/classificationRule.py
@@ -2,7 +2,7 @@ import ifcopenshell
 
 def checkRule(model):
     doors = model.by_type('IfcDoor')
-
+    print('cool')
     #result = f"Doors: {len(doors)}"
     result = 'are you sure you are using a classification system?'
     

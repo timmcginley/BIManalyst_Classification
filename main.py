df324ae8691a5f62b950232cbcdaad6dee984adb
Tim McGinley <timwasnever@gmail.com>
2024-09-12 13:43:12 +0200

    add classificationRule

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import ifcopenshell
 
-from rules import windowRule
-from rules import doorRule
+#from rules import windowRule
+#from rules import doorRule
+from rules import classificationRule
 
-model = ifcopenshell.open("path/to/ifcfile.ifc")
+model = ifcopenshell.open("C:/models/24_06/CES_BLD_24_06_ARC.ifc")
 
-windowResult = windowRule.checkRule(model)
-doorResult = doorRule.checkRule(model)
+#windowResult = windowRule.checkRule(model)
+#doorResult = doorRule.checkRule(model)
+classificationResult = classificationRule.checkRule(model)
 
-print("Window result:", windowResult)
-print("Door result:", doorResult)
+#print("Window result:", windowResult)
+#print("Door result:", doorResult)
+print("Classification result:", classificationResult)
--- rules/classificationRule.py
+++ rules/classificationRule.py
@@ -1,3 +1,9 @@
 import ifcopenshell
 
-print('are you sure you are using a classification system?')
+def checkRule(model):
+    doors = model.by_type('IfcDoor')
+
+    #result = f"Doors: {len(doors)}"
+    result = 'are you sure you are using a classification system?'
+    
+    return result
